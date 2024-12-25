f55e29dcbf216138fb0f325ae478b3eea6b93957
Robin Mattern <robin.mattern@gmail.com>
2024-12-25 12:41:04 -0500

    .(41225.04_Make sure RSS22_DirList file is updated

--- ._2/JPTs/RSS/dirList/RSS22_DirList.sh
+++ ._2/JPTs/RSS/dirList/RSS22_DirList.sh
@@ -6,7 +6,8 @@
 #       RSS_Scr=RSS22-DirList_v1.3.21027.sh
 #       RSS_Scr=RSS22-DirList_p1.04.sh
 #       RSS_Scr=RSS22-DirList_p1.05.sh  # 40520.1002
-        RSS_Scr=RSS22_DirList_p1.05.sh  # 41026.1535
+#       RSS_Scr=RSS22_DirList_p1.05.sh  # 41026.1535
+        RSS_Scr=RSS22_DirList_u1.05.sh  # 41225.1030
 
         RSS_Dir=$( dirname "$0" )
         RSS22_DirList=$RSS_Dir/$RSS_Scr
