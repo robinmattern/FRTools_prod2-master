999e504cade687dedf48291d85a618a6ba5283fd
Robin Mattern <robin.mattern@gmail.com>
2025-01-05 12:41:48 -0500

    .(50105.04_Fix install.sh call of re-install install $1

--- ._2/ZIPs/set-repos-dir/install.sh
+++ ._2/ZIPs/set-repos-dir/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-  ./re-install $1 install                                    # .(50105.04.1 RAM Add install)
+  bash re-install install $1                                    # .(50105.04.1 RAM Add install)
 
   
