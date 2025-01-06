2b5e5982833c2910b5c8fc6918efc7214c6b97df
Robin Mattern <robin.mattern@gmail.com>
2025-01-05 21:17:37 -0500

    .(50105.05_Polish the final msg for set-aicoder.sh (c)

--- ._2/ZIPs/set-repos-dir/install-aicoder.sh
+++ ._2/ZIPs/set-repos-dir/install-aicoder.sh
@@ -4,7 +4,7 @@
 #  ----------------------------------------------------------------------------
    export aQuiet=q                                                                      # .(50105.05b.6 RAM Add aQuiet)
 
-   frt install ${aRepo} $2 -dq;                                                         
+   frt install ${aRepo} $2 -d;                                                          # .(50105.05b.7)
 
 #  echo -e "\n  cd ${aRepo}/client1"                                                    ##.(50104.01.10)
 #  cd ${aRepo}/client1
@@ -15,10 +15,12 @@
 #  cp -p ./._2/FRTs/AICodeR/metadata/_env.example ./._2/FRTs/AICodeR/_env               ##.(50104.01.12)
 
 #  echo -e "\n  Edit SERVER_HOST and {LLM}_API_KEYs in _env:"                           ##.(50104.01.13 Beg)
-#  echo -e "      cd ${aRepo}"
-#  echo -e "      nano client1/c16_aidocs-review-app/utils/FRTs/_env"
-#  echo -e "      ./run-client.sh"       
-#  echo -e "  or  code *code* for VSCode"                                               ##.(50104.01.13 End)   
+#  echo -e   "     cd ${aRepo}"
+#  echo -e   "     nano client1/c16_aidocs-review-app/utils/FRTs/_env"
+#  echo -e   "     ./run-client.sh"
+#  echo -e   "  or run code *code* for VSCode"                                          ##.(50104.01.13 End)
 
-   if [ "${OS:0:7}"     != "Windows" ]; then echo ""; fi 
-   
+   echo -e   "  Please change into the project folder: cd ${aRepo}"                     # .(50105.05c.1)
+   echo -e   "  You can now work on it in VSCode with: code *code*"                     # .(50105.05c.2)
+
+   if [ "${OS:0:7}"     != "Windows" ]; then echo ""; fi
