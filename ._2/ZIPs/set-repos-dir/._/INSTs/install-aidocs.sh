900239377da9a43ab80c1c6dc7565d66f31521d1
Robin Mattern <robin.mattern@gmail.com>
2025-04-06 12:45:46 -0400

    .(50406.01.1_Don't display AnyLLM msg for dev01

--- ._2/ZIPs/set-repos-dir/._/INSTs/install-aidocs.sh
+++ ._2/ZIPs/set-repos-dir/._/INSTs/install-aidocs.sh
@@ -14,7 +14,7 @@
 #  aRepo="$1"; if [ "$1" == "" ]; then aRepo="AIDocs_demo1"; fi                         # .(50402.15.5)
 
    aAppDir="$1"; aAccount="$2"; # echo -e "\n  \$1: '$1', \$2: '$2'"; aAppDir="$1"; aAccount="$2"
-   if [ "${aAppDir/_}" != "${aAppDir}" ]; then aStage="${aAppDir/*_}"; aAppDir="${aAppDir/_*/}"; fi  # .(50402.15.2 RAM Split at _)
+   if [ "${aAppDir/_}" != "${aAppDir}" ]; then aStage="${aAppDir/*_}";aAppDir="${aAppDir/_*/}"; fi  # .(50402.15.2 RAM Split at _)
 
                                        aRepo="$1";                  aAppDir="$1"        # .(50402.15.x RAM Seems strange, but ...)
    if [ "${aStage}" == ""      ]; then aRepo="AIDocs_demo1-master"; aAppDir="no-stage"; fi
@@ -33,7 +33,7 @@
 #          frt install  ${aRepo}  $ {aAppDir}  ''   $3;                                 # .(50105.05b.8 RAM Remove -q)
 
 #          frt clone  {RepoName}  ''  {CloneDir} {Branch} {Account}
-#  echo "  frt clone   '${aRepo}' '' '${aAppDir}'    '${aAccount}' -d;"; # exit
+   echo "  frt clone   '${aRepo}' '' '${aAppDir}'    '${aAccount}' -d;"; # exit
 #          frt clone    ${aRepo}  ''  ${aAppDir}  ''  ${aAccount}    ;   # exit
 #          frt clone    ${aRepo}  ''  ${aAppDir}  ''  ${aAccount}  -b;     exit
 
@@ -80,50 +80,61 @@
 if [ -d "${aRepoDir}/client1" ]; then
 
 if [ -f "${aRepoDir}/._2/package.json" ]; then                                          # .(50402.20.1 RAM Run other npm installs Beg)
-#  echo -e "  cd ${aRepoDir}/._2";   exit                                               ##.(50106.06.6).(50104.01.10)
+   echo -e "  cd ${aRepoDir}/._2";  # exit                                              ##.(50106.06.6).(50104.01.10)
      cd "${aRepoDir}/._2"
            npm install >/dev/null
-     cd ..
+     cd ../../
      fi
 #   --------------------------------------------------
 
 if [ -f "${aRepoDir}/server1/package.json" ]; then
-   echo -e "  cd ${aRepoDir}/client1" # exit                                            ##.(50106.06.6).(50104.01.10)
+   echo -e "  cd ${aRepoDir}/server1" # exit                                            ##.(50106.06.6).(50104.01.10)
      cd "${aRepoDir}/server1"
            npm install >/dev/null
-     cd ..
+     cd ../../
      fi                                                                                 # .(50402.20.1 End)
 #   --------------------------------------------------
 
-#    cd "${aRepoDir}/client1"                                                           # .(50106.06.7)
+if [ -f "${aRepoDir}/client1/package.json" ]; then
    echo -e "  cd ${aRepoDir}/client1" # exit                                            ##.(50106.06.6).(50104.01.10)
+     cd "${aRepoDir}/client1"                                                           # .(50106.06.7)
    echo "  npm install"
 
-if [ -f "${aRepoDir}/client1/package.json" ]; then
            npm install | awk '{ print "    " $0 }'
-     fi
+
+     fi # eif ${aRepoDir}/client1/package.json
 # --------------------------------------------------------------
 
-if [ -f "./c16_aidocs-review-app/utils/FRTs/_env_local-local.txt" ]; then
-   cp -p ./c16_aidocs-review-app/utils/FRTs/_env_local-local.txt ./c16_aidocs-review-app/utils/FRTs/_env
-   fi
+function cpyEnv() {                                                                                         # .(50406.03.1 RAM Copy _env Beg)
+      aFrom="$1/$2"; aTo="$1/$3"       
+      if [ -f "${aFrom}" ]; then cp -p "${aFrom}" "${aTo}"; fi
+      }                                                                                                     # .(50406.03.1 End)
+# --------------------------------------------------------------
 
-   if [ "${bAnyLLM}" == "1" ]; then                                                     # .(50402.15.7 RAM Only set ANYLLM_KEY id it's installed)
+#if [  -f "${aRepoDir}/client1/c16_aidocs-review-app/utils/FRTs/_env_local-local.txt" ]; then   
+#   cp -p "${aRepoDir}/client1/c16_aidocs-review-app/utils/FRTs/_env_local-local.txt"  "${aRepos}/client1/c16_aidocs-review-app/utils/FRTs/_env"
+#   fi
+   cpyEnv "${aRepoDir}/client1/c16_aidocs-review-app/utils/FRTs"  "_env_local-local.txt"  "_env"            # .(50406.03.2 RAM Copy c16 _env) 
+   cpyEnv "${aRepoDir}/server1/s12_websearch-app" ".env_example"  ".env"                                    # .(50406.03.2 RAM Copy s12 .env)
+
+# --------------------------------------------------------------
+
+   if [ "${bAnyLLM}" == "1" ] && [ "${aStage}" != "dev01" ]; then                       # .(50406.01.1).(50402.15.7 RAM Only set ANYLLM_KEY id it's installed)
 #  echo ""
    echo -e "\n  Edit SERVER_HOST and ANYLLM_API_KEY in _env:"
    echo -e   "     cd ${aRepoDir}"                                                      # .(50106.06.8
    echo -e "      nano client1/c16_aidocs-review-app/utils/FRTs/_env"
    echo -e "     ./run-client.sh\n"
-   echo -e   "  or work on it in VSCode with: code ${aRepo/\//}*"                       # .(50105.05c.7).(50106.06.8).(50104.01.13 End)
+   echo -e   "  or work on it in VSCode with: code ${aRepoDir/\//}*"                    # .(50105.05c.7).(50106.06.8).(50104.01.13 End)
    fi                                                                                   # .(50106.04.16 RAM Exit if bDoit=0)
 
-  else                                                                                  # .(50402.15.8)
-    echo -e "* AIDocs didn't get installed into folder: ${aRepoDir}/client1.";          # .(50402.15.9)
-#   exit
-   fi                                                                                   # .(50402.15.10)
-   fi
+ else                                                                                  # .(50402.15.8)
+   echo -e "* AIDocs didn't get installed into folder: ${aRepoDir}/client1.";          # .(50402.15.9)
+#  exit
+   fi # eif ${aRepoDir}/client1
+ fi  # eif $? -ne 1                                                                   # .(50402.15.10)
 
-   if [ "${OS:0:7}"     != "Windows" ]; then echo ""; fi
+  if [ "${OS:0:7}"     != "Windows" ]; then echo ""; fi
 
 # frt clone AIDocs_demo1-master ''  ''            -->   AIDocs_demo1-master.git  AIDocs                x   AIDocs_demo1-master
 # frt clone AIDocs_demo1-master ''  demo1         -->   AIDocs_demo1-master.git  AIDocs_demo1
