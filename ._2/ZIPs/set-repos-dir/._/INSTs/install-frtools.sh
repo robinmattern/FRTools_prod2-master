1f0ea95f4f53d06a81a7d10aff4241a531b1561c
Robin Mattern <robin.mattern@gmail.com>
2025-08-24 14:01:23 -0400

    .(50810.01_Check if .git exists

--- ._2/ZIPs/set-repos-dir/._/INSTs/install-frtools.sh
+++ ._2/ZIPs/set-repos-dir/._/INSTs/install-frtools.sh
@@ -7,10 +7,12 @@
    aRepo="$1"; if [ "$1" == "" ]; then aRepo="FRTools"; fi
 #  ----------------------------------------------------------------------------
 
+   if [ "$( which git )" == "" ]; then echo -e "\n* You must install git, first\n"; exit; fi   # .(50810.01.1 RAM Check for git)
+
    git clone https://github.com/robinmattern/FRTools_prod2-master.git "${aRepo}"
 
    cd ${aRepo}
 
    if [ "${OS}" != "Windows" ]; then sudo chmod 755 *.sh; fi
-   
+
    ./set-frtools.sh doit
