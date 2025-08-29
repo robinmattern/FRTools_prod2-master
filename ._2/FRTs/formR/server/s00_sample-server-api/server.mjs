d29c5dbf1399a4facb543fcf1ceb91f5294dcf44
Robin Mattern <robin.mattern@gmail.com>
2025-08-29 10:15:47 -0400

    .(50829.02_Modify index.html and server.mjs

--- ._2/FRTs/formR/server/s00_sample-server-api/server.mjs
+++ ._2/FRTs/formR/server/s00_sample-server-api/server.mjs
@@ -2,14 +2,14 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-const PORT = 3250;
+const nPort = 3250;
 
-app.use(cors());
+app.use( cors() );
 
 app.get('/api', (req, res) => {
-  res.send('<h4 style="margin:-10px 0 0 25px;">.. from the formR s00_Sample-Server-API at localhost:3250/api</h4>');
-});
+  res.send( `<h4 style="margin:-10px 0 0 20px;">.. from the formR s00_Sample-Server-API at 127.0.0.1:${nPort}/api</h4>` );
+  });
 
-app.listen(PORT, () => {
-  console.log(`  Server running at http://localhost:${PORT}/api`);
-});
+app.listen( nPort, () => {
+//console.log(`  Server running at http://localhost:${nPort}/api`);
+  });
