b1c6f25a35e6ae26bb09fc6ccc4f6397fb151df7
Robin Mattern <robin.mattern@gmail.com>
2025-11-23 18:35:24 -0500

    .(50920.01_Add commit list to commit

---
+++ ._2/FRTs/gitR/FRT48_commit1.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#  FRTools Launch Script
+
+    aCmd="$( dirname $0)/FRT48_commit1_u1.11.sh"
+
+   "${aCmd}" "$@"
+
--- ._2/FRTs/gitR/FRT48_commit1_u1.11.sh
+++ ._2/FRTs/gitR/FRT48_commit1_u1.11.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
+#*\
+##=========+====================+================================================+
+##RD         Commit            | GitR Tools
+##RFILE    +====================+=======+===============+======+=================+
+##FD   FRT48_Commit.sh          |       |  9/08/25  8:15|      | p1.02`50908.0815
+
+##DESC     .--------------------+-------+---------------+------+-----------------+
+#            Easy commit 
+#
+##LIC      .--------------------+----------------------------------------------+
+#            Copyright (c) 2022-2025 JScriptware * Released under
+#            MIT License: http://www.opensource.org/licenses/mit-license.php
+##FNCS     .--------------------+----------------------------------------------+
+#            commit             | 
+##CHGS     .--------------------+----------------------------------------------+
+#.(50908.01   9/08/25 RAM  8:15a| Add commit
+#.(50909.01   9/09/25 RAM  7:40a| Add Help
+#.(50912.01   9/12/25 RAM  6:50p| Add -num 2nd arg
+#.(50917.01   9/17/25 RAM  7:40a| Also check for .git
+#.(50918.01   9/18/25 RAM 11:00a| Must include email
+#.(50920.01   9/20/25 RAM  9:06a| Add commit list
+
+##PRGM     +====================+===============================================+
+##ID 69.600. Main0              |
+##SRCE     +====================+===============================================+
+#*/
+#========================================================================================================== #  ===============================  #
+
 
   if [ "$1" == "help" ] || [  "${1:0:2}" == "-h" ] || [  "$1" == "" ]; then                                           # .(50909.01.1 RAM Add help Beg)
      echo -e "\n  Usage: commit \"Commit Message\" [author]";
@@ -9,13 +37,14 @@
      echo -e "     author:'me' (git user.name) is optional. Default is 'Amazon Q'";
      echo -e "        set: sets next {CommitNo} or {YMMDD}.{CommitNo}"
      echo -e "       show: show commit.no saved in ./docs/commit.no"
+     echo -e "       list: list current commits, or -local, -remote, -both, -wworking, -staged {nCnt}"
      echo -e "         -k: keep commit.no. -k must be before message"
      echo -e "         -n: use  commit.no. -n YMMDD.NN must be before message"
      if [ "${OS:0:3}" != "Win" ]; then echo ""; fi
      exit
      fi                                                                                                               # .(50909.01.1 End)
         
-  if [ ! -d ".git" ] || [ ! -d "docs" ]; then                                                                         # .(50917.01b.1 RAM Also check for .git).(50917.01.1 RAM Add check Beg)
+  if [ ! -d ".git" ] || [ ! -d "docs" ]; then                                                                         # .(50917.01.1 RAM Also check for .git).(50917.01.1 RAM Add check Beg)
      echo -e "\n* You must be in the repo's root dir to use commit."
      if [ "${OS:0:3}" != "Win" ]; then echo ""; fi  
      exit; fi                                                                                                         # .(50917.01.1 End)
@@ -28,6 +57,17 @@
      if [ "${OS:0:3}" != "Win" ]; then echo ""; fi
      exit
      fi
+  if [ "$1" == "list" ]; then                                                                                         # .(50920.01.1 RAM Add commit list Beg)
+     if [ "$2" == ""           ]; then gitr list commit; fi 
+     if [ "${2:0:2}" == "-l"   ]; then gitr list commit $3; fi 
+     if [ "${2:0:2}" == "-r"   ]; then gitr list commit $3 remote; fi 
+     if [ "${2:0:2}" == "-b"   ]; then gitr list commit $3 both; fi 
+     if [ "${2:0:2}" == "-w"   ]; then gitr list workin $3; fi 
+     if [ "${2:0:2}" == "-s"   ]; then gitr list staged $3; fi 
+     if [ "${OS:0:3}" != "Win" ]; then echo ""; fi
+     exit
+     fi                                                                                                               # .(50920.01.1 End)
+
      aTS=$( date '+%y%m%d' ); aTS="${aTS:1}"
 
   if [ ! -f "${aCommitFile}" ]; then echo "${aTS}.00" >"${aCommitFile}" ; fi
@@ -35,6 +75,8 @@
 
      nKeepNo=0; if [ "${2:0:2}" == "-n" ]; then nKeepNo=$3; set -- "$1" "${@:4}"; fi                                  # .(50912.01.1 RAM Add -num 2nd arg)
                 if [ "${1:0:2}" == "-n" ]; then nKeepNo=$2; shift; shift; fi                                          # .(50912.01.2 RAM Add -num as 1st arg)
+ #        echo -e "\n1.  \$1: '$1', \$2: '$2', nKeepNo: '$nKeepNo'"; # exit 
+
   if [ "${nKeepNo}" != "0" ];  then aCommitNo=${nKeepNo};                                                             # .(50912.01.3)
 #    echo -e "\n  Use  commit.no: '.(${aCommitNo}' -- ${nKeepNo}";
    else
@@ -46,6 +88,7 @@
      fi; # echo "${aTS}             # .(50908.01.3 RAM Add bKeeper)
 
      aDay="${aCommitNo:0:5}";    if [ "${aDay}" != "${aTS}" ] && [ "${bKeeper}" != "1" ]; then aCommitNo="${aTS}.00"; fi # .(50908.01.4)
+#    echo -e "2.  \$1: '$1', \$2: '$2', nKeepNo: '$nKeepNo'"; # exit 
 
   if [ "$1" == "set" ]; then
      n=$2; if [ "${n:5:1}" == "." ]; then aTS=${n:0:5}; n=${n:6:2}; fi
@@ -69,11 +112,14 @@
      fi # eof nKeepNo
 
   if git diff --cached --quiet; then aAdd="git add . && "; else aAdd=""; fi
+#    echo -e "3.  \$1: '$1', \$2: '$2', aAuthor: '${aAuthor}'"; # exit 
 
 # if [ "$2" == "robin"]; then aAuthor=""; else aAuthor="--author \"Agent Q <agent@amazon.com>\""; fi
 # if [ "$2" != "" ]; then aAuthor="$( git config list | awk '/user.name/ { print substr($0,11); exit }' )";           ##.(50918.01.1 RAM Must include email)
-  if [ "$2" != "" ]; then aAuthor="$( git config list | awk '/user.name/ { n=substr($0,11) }; /user.email/ { m=substr($0,12) }; END { print n " <" m ">" }' )" # .(50918.01.1)
+  if [ "$2" != "" ]; then aAuthor="$( git config list | awk '/user.name/ && !/Amazon/ { n=substr($0,11) }; /user.email/ { m=substr($0,12) }; END { print n " <" m ">" }' )" # .(50918.1b.1 RAM Add !/Amazon/).(50918.01.1)
                      else aAuthor="Amazon Q <agent@amazon.com>"; fi
+#    echo -e "4.  \$1: '$1', \$2: '$2', aAuthor: '${aAuthor}'"; # exit 
+
   if [ "$1" == "" ]; then
      echo -e "\n* Enter a commit \"message\".\n"
      if [ "${OS:0:3}" != "Win" ]; then echo ""; fi
